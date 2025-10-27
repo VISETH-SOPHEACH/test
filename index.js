69bad6716cdd40318d3f4ab81fe7f48821d86b69
viseth-max <visethsopheach@gmail.com>
2025-10-27 15:03:02 +0700

    master

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonserver = require("json-server");
+const server = jsonserver.create();
+const cors = require('cors');
+const router = jsonserver.router("db.json");
+const middlewares = jsonserver.defaults();
+const port = process.env.PORT || 8000;
+
+// Add static file serving for images
+server.use('/images', jsonserver.static('./images'));
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
